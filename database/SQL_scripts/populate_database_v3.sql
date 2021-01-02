9ff20e720b356516eca82836ca8421d735e23c64
jaigarV <jaimevusa@hotmail.com>
2021-01-02 23:37:36 +0100

    Fix typo in prepopulated customers name

--- database/SQL_scripts/populate_database_v3.sql
+++ database/SQL_scripts/populate_database_v3.sql
@@ -1,8 +1,8 @@
 -- Insert deafult values in all tables to perform tests
 INSERT INTO `ecommerce_database`.`ADMINISTRATOR` (`IdentityNumber`, `Name`, `Password`, `Email`, `Phone`) VALUES ('123456789', 'TestAdmin', 'test', 'TestAdmin@test.com', '123456');
 
-INSERT INTO `ecommerce_database`.`CUSTOMER` (`IdentityNumber`, `Name`, `Password`, `Email`, `Phone`, `Address`, `Gender`, `Age`, `Country`) VALUES ('123456789', 'TestCutomer', 'test', 'TestCutomer@test.com', '123456', 'Test Street, Number 0, 000 00, Lulea', 'Other', '0', 'World');
-INSERT INTO `ecommerce_database`.`CUSTOMER` (`IdentityNumber`, `Name`, `Password`, `Email`, `Phone`, `Address`, `Gender`, `Age`, `Country`) VALUES ('999999999', 'TestCutomer2', 'test', 'TestCutomer2@test.com', '654321', 'Test Street, Number 2, 000 00, Lulea', 'Other', '200', 'Space');
+INSERT INTO `ecommerce_database`.`CUSTOMER` (`IdentityNumber`, `Name`, `Password`, `Email`, `Phone`, `Address`, `Gender`, `Age`, `Country`) VALUES ('123456789', 'TestCustomer', 'test', 'TestCutomer@test.com', '123456', 'Test Street, Number 0, 000 00, Lulea', 'Other', '0', 'World');
+INSERT INTO `ecommerce_database`.`CUSTOMER` (`IdentityNumber`, `Name`, `Password`, `Email`, `Phone`, `Address`, `Gender`, `Age`, `Country`) VALUES ('999999999', 'TestCustomer2', 'test', 'TestCutomer2@test.com', '654321', 'Test Street, Number 2, 000 00, Lulea', 'Other', '200', 'Space');
 
 INSERT INTO `ecommerce_database`.`SELLER` (`OrganizationID`, `Name`, `Password`, `Email`, `Phone`, `Address`, `Country`) VALUES ('1223344', 'TestSeller', 'test', 'TestSeller@test.com', '00112233', 'Test factory, Number 101, 111 11, Gothenburg', ' Sweden');
 INSERT INTO `ecommerce_database`.`SELLER` (`OrganizationID`, `Name`, `Password`, `Email`, `Phone`, `Address`, `Country`) VALUES ('4433221', 'TestSeller2', 'test', 'TestSeller2@test.com', '33221100', 'Test factory, Number 102, 111 12, Stockholm', ' Sweden');
